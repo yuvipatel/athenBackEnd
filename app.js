1b04db49875b45d78ebdd8a03796c955e68aaf06
sandip.nirmal <sandip.nirmal@synerzip.com>
2017-02-08 13:58:50 +0530

    Add TSL support for API

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-let routes = require("./routes.js");
-let express = require('express');
-let app = express();
+const routes = require("./routes.js");
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
 routes(app);
 
-app.listen(3000, () => {
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
--- routes.js
+++ routes.js
@@ -9,16 +9,17 @@ function routes(app) {
 
     app.use(cors());
     // parse application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
 
     app.use(bodyParser.json());
 
     app.use(errorHandler);
 
     app.post('/processData', processDataHandler);
+
 }
 
 
 module.exports = exports = routes;
-
-
