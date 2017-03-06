f1bf2a110e31615b6350d37abaca029f793c8a77
sandip.nirmal <sandip.nirmal@synerzip.com>
2017-03-06 14:30:46 +0530

    Increasing search result limit from 50 to 100

--- handlers/eUtilsHandler.js
+++ handlers/eUtilsHandler.js
@@ -61,7 +61,7 @@ function getSearchResult(req, res, next) {
             .then((d) => {
                 //supported eutil parameters can be added like this
                 // d.retstart = 5;
-                d.retmax = 50;
+                d.retmax = 100;
                 return eutils.esummary(d);
             })
             .then((d) => {
